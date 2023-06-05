0ae720134421bc535cd88750faf15844b2733779
joshvin144 <joshvin144@gmail.com>
2023-06-04 19:52:01 -0700

    Added results_dict to worker_execution functions; set threads as daemons

--- main.py
+++ main.py
@@ -22,13 +22,16 @@ message_queue = queue.Queue()
 def receiver_1_worker_execution(results_dict = None):
 	message = message_queue.get()
 	print(f"Reciever 1 retrieved a message from the message queue: {message}")
-	if(None != results_dict)
+	if(None != results_dict):
+		results_dict["receiver_1_results"].append(message)
 
 # Helper function for receiver_2_worker
 @utilities.function_run_time
 def receiver_2_worker_execution(results_dict = None):
 	message = message_queue.get()
 	print(f"Reciever 2 retrieved a message from the message queue: {message}")
+	if(None != results_dict):
+		results_dict["receiver_2_results"].append(message)
 
 # Loop to continuously check the queue for messages
 # If we wrap the receiver_1_worker in the utilities.function_run_time decorator,
@@ -57,23 +60,23 @@ def main(arg = None, argv = None):
 	# Results dictionary to pass between threads
 	results_dict = {"receiver_1_results" : [], "receiver_2_results" : []}
 
-	# Thread that receives signals
-	receiver_1_thread = threading.Thread(target = receiver_1_worker)
-	receiver_2_thread = threading.Thread(target = receiver_2_worker)
+	# Threads to receive messages
+	receiver_1_thread = threading.Thread(target = receiver_1_worker, daemon = True)
+	receiver_2_thread = threading.Thread(target = receiver_2_worker, daemon = True)
 
-	# Extracting information from a thread takes a significant amount of time
-	# receiver_1_thread = threading.Thread(target = receiver_1_worker, args = (results_dict,))
-	# receiver_2_thread = threading.Thread(target = receiver_2_worker, args = (results_dict,))
+	# receiver_1_thread = threading.Thread(target = receiver_1_worker, args = (results_dict,), daemon = True)
+	# receiver_2_thread = threading.Thread(target = receiver_2_worker, args = (results_dict,), daemon = True)
 	
 	receiver_1_thread.start()
 	receiver_2_thread.start()
 
 	# Send messages
-	while(True):
-		message = "New Message!"
+	for i in range(NUM_MESSAGES):
+		message = f"Message {i}"
 		message_queue.put(message)
 		time.sleep(TIME_BETWEEN_MESSAGES)
 
+    print(results_dict)
 	return 0
 
 if __name__ == '__main__':
